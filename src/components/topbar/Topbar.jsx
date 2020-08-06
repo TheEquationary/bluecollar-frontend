1978d2ef23e0d4b5871577a3bc2c1794e7b643a4
shubham4443 <shubham4443@gmail.com>
2020-08-07 02:58:14 +0530

    responsive topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,6 +7,7 @@ import LogoBlueCollar from "../../assets/logo-bluecollar.png";
 // constants
 import { menuKeys } from "../../constants";
 // antd
+import { MenuOutlined } from '@ant-design/icons';
 import { Space, Menu } from "antd";
 const { SubMenu } = Menu;
 
@@ -14,6 +15,7 @@ const Topbar = () => {
 
   const history = useHistory();
   const currentMenuKey = useSelector(state => state.uiState.currentMenuKey);
+  const screenBreakpoint = useSelector(state => state.uiState.screenBreakpoint);
 
   const handleMenuClick = (key) => {
     console.log(key);
@@ -30,7 +32,7 @@ const Topbar = () => {
           </span>
         </Space>
         <Space>
-          <Menu onClick={handleMenuClick} selectedKeys={currentMenuKey} mode="horizontal">
+          <Menu onClick={handleMenuClick} selectedKeys={currentMenuKey} mode="horizontal" className="topbar_menu">
             <Menu.Item key={menuKeys.HOME} onClick={() => history.push("/login")}>
               <b>Home</b>
             </Menu.Item>
@@ -42,6 +44,7 @@ const Topbar = () => {
               <b>About Us</b>
             </Menu.Item>
           </Menu>
+          <span className="material-icons topbar_menu_icon">menu</span>
         </Space>
       </div>
     </div>

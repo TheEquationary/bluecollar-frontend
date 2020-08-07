21f395a91fee68b58969d75df1462be2b7c53662
shubham4443 <shubham4443@gmail.com>
2020-08-08 01:48:57 +0530

    made topbar responsive and added drawer for mobile view

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./topbar.css";
 import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -7,18 +7,33 @@ import LogoBlueCollar from "../../assets/logo-bluecollar.png";
 // constants
 import { menuKeys } from "../../constants";
 // antd
-import { MenuOutlined } from '@ant-design/icons';
-import { Space, Menu } from "antd";
+import { Space, Menu, Drawer } from "antd";
 const { SubMenu } = Menu;
 
 const Topbar = () => {
 
   const history = useHistory();
-  const currentMenuKey = useSelector(state => state.uiState.currentMenuKey);
-  const screenBreakpoint = useSelector(state => state.uiState.screenBreakpoint);
 
+  // Global state 
+  const currentMenuKey = useSelector(state => state.uiState.currentMenuKey);
+
+  // Component state
+  const [drawer, setDrawer] = useState(false);
+
+  // constants
+  const titleAndTagline =
+    <React.Fragment>
+      <b>BlueCollar</b><br />
+      <i>Handymen and Rennovation Services</i>
+    </React.Fragment>
+
+  // handlers
   const handleMenuClick = (key) => {
     console.log(key);
+  }
+
+  const handleSideMenuIconClick = () => {
+    setDrawer(true);
   }
 
   return (
@@ -27,8 +42,7 @@ const Topbar = () => {
         <Space>
           <img src={LogoBlueCollar} alt="logo.png" className="logo" />
           <span className="title-tagline">
-            <b>BlueCollar</b><br />
-            <i>Handymen and Rennovation Services</i>
+            {titleAndTagline}
           </span>
         </Space>
         <Space>
@@ -44,9 +58,36 @@ const Topbar = () => {
               <b>About Us</b>
             </Menu.Item>
           </Menu>
-          <span className="material-icons topbar_menu_icon">menu</span>
+          <span className="material-icons topbar_menu_icon" onClick={handleSideMenuIconClick}>menu</span>
         </Space>
       </div>
+      {drawer &&
+        <Drawer
+          title={titleAndTagline}
+          placement="top"
+          closable={true}
+          onClose={() => setDrawer(false)}
+          visible={drawer}
+          height="auto"
+        >
+          <Menu
+            onClick={handleMenuClick}
+            style={{ width: "100%" }}
+            selectedKeys={currentMenuKey}
+            mode="inline"
+          >
+            <Menu.Item key={menuKeys.HOME} onClick={() => history.push("/login")}>
+              <b>Home</b>
+            </Menu.Item>
+            <SubMenu title={<b>Services</b>}>
+              <Menu.Item key={menuKeys.SERVICES.ELECTRICIAN}>Electrician</Menu.Item>
+              <Menu.Item key={menuKeys.SERVICES.PLUMBER}>Plumber</Menu.Item>
+            </SubMenu>
+            <Menu.Item key={menuKeys.ABOUT_US} onClick={() => history.push("/about-us")}>
+              <b>About Us</b>
+            </Menu.Item>
+          </Menu>
+        </Drawer>}
     </div>
   )
 }

d1b8368e3474cd1a0e259687a9d9ab36b6503e43
shubham4443 <shubham4443@gmail.com>
2020-08-08 02:11:36 +0530

    changed favicons and added a demo flipping card

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { FlippingCard, FlippingCardFront, FlippingCardBack } from "react-ui-cards";
 // components
 import Topbar from "../../components/topbar/Topbar";
-import {Content} from "../../components/page-layout/Layout";
+import { Content } from "../../components/page-layout/Layout";
 
 const Home = () => {
   return (
     <React.Fragment>
       <Topbar />
       <Content>
-        <h1>Hello World</h1>
+        <FlippingCard>
+          <FlippingCardBack>
+            Content that will be displayed on the back of the card
+  	      </FlippingCardBack>
+          <FlippingCardFront>
+            Content that will be displayed on the front of the card
+  	      </FlippingCardFront>
+        </FlippingCard>
       </Content>
     </React.Fragment>
   )
